b4c8fe89dc0a26d08331c92e5228d08beac1eba2
adamriera <adam.riera@yahoo.es>
2023-12-20 18:00:34 +0100

    Todo OK

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -9,19 +9,19 @@ export class StepsService {
     {
       title: 'Dedica moltes hores',
       description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      img: 'assets/time_managment.svg',
+      img: 'assets/images/time_managment.svg',
       bgcolor: '#ffffff',
     },
     {
       title: 'Programa projectes propis',
       description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      img: 'assets/programming.svg',
+      img: 'assets/images/programming.svg',
       bgcolor: '#f0f0f0',
     },
     {
       title: 'Procura descansar',
       description: 'Descansar bé i desconnectar són vitals. D\'aquesta manera reduiràs l\'estrès i l\'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.',
-      img: 'assets/meditation.svg',
+      img: 'assets/images/meditation.svg',
       bgcolor: '#e0e0e0',
     },
   ];
